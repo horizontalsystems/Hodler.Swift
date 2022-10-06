ce19fe491022d17c27fee3fe38c4991a6951952b
ant013 <ant013@mail.ru>
2022-10-06 12:05:32 +0600

    Extract Hodler.Swift
    Remove all other files.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Hodler",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "Hodler",
+            targets: ["Hodler"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "Hodler",
+            dependencies: [
+                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
+                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/Hodler/Classes/Core/Protocols.swift
@@ -0,0 +1,14 @@
+import Foundation
+import BitcoinCore
+
+public protocol IHodlerAddressConverter {
+    func convert(keyHash: Data, type: ScriptType) throws -> Address
+}
+
+public protocol IHodlerPublicKeyStorage {
+    func publicKey(byRawOrKeyHash hash: Data) -> PublicKey?
+}
+
+public protocol IHodlerBlockMedianTimeHelper {
+    var medianTimePast: Int? { get }
+}
